ef8b769b31cda7f3819bb450094b45a1670f0ebf
mlisbao <mlisbao@hotmail.com>
2024-09-16 21:26:22 -0300

    whatsapp

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
-import requests
 import streamlit as st
+import datetime
+import pywhatkit as py
 
-data = requests.get("https://intranet.portodesantos.com.br/_json/operadores.asp").json()
+phonelist = st.text_input("Please input numbers you want to automate with seperated by ,")
+message = st.text_area("Enter your Message")
+submit = st.button("Enter")
 
-st.write(data)
+i=1
+if submit:
+phone = phonelist.split(',')
+for each in phone:
+py.sendwhatmsg("+55"+each,message,time.hour,(time.minute+i),10)
+i=i+1
+st.success("Message Sent!")
